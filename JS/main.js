9adcb4c15291a8aa25a82efdbf68e4b01e52bbd9
Pius <mutundu.nguma@gmail.com>
2022-11-03 21:16:38 +0300

    convert marks into percentage

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,11 @@
 const submitAnswer = document.querySelector("#submit_ans")
 const quizAnswers = document.querySelectorAll(".ouranswers");
 let marks = 0;
+let total = 0;
 quizAnswers.forEach(function (jibu) {
+    if (parseInt(jibu.value) > 0) { total += parseInt(jibu.value); }
+
+    else { total = total; }
     jibu.addEventListener("change", function (event) {
         marks += parseInt(event.target.value)
 
@@ -10,5 +14,6 @@ quizAnswers.forEach(function (jibu) {
 });
 //alert(marks)
 submitAnswer.addEventListener("click", function (event) {
-    alert("your score is:" + marks)
+    let score =(marks/total)*100;
+    alert("your score is:" + score +"%")
 })
